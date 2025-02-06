80d93c3b0feed040659e4be11ef4a5a280b3f6fd
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2025-02-06 07:42:57 +0700

    update footer

--- resources/views/admin/components/footer.blade.php
+++ resources/views/admin/components/footer.blade.php
@@ -3,5 +3,8 @@
         <div class="col-auto">
             <p class="mb-0">© {{ date('Y') }} SIPARMA. All Rights Reserved.</p>
         </div>
+        {{--  <div class="col-auto">
+            <p class="mb-0">Made by <span class="text-primary-600">pixcelsthemes</span></p>
+        </div>  --}}
     </div>
 </footer>
