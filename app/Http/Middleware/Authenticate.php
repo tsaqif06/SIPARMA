b7a3020a9c2180585f31a4ef0fc0e0887a313580
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2025-02-15 16:37:54 +0700

    middleware improvement

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,14 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        if ($request->expectsJson()) {
+            return null;
+        }
+
+        if ($request->is('admin') || $request->is('admin/*')) {
+            return route('admin.login');
+        }
+
+        return route('login');
     }
 }
