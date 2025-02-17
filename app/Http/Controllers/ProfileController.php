17d4119a5d7193d9d9e1c0a2412185150110a7b4
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2025-02-17 10:18:29 +0700

    update transaction history page

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -67,8 +67,8 @@ class ProfileController extends Controller
         $user = auth()->user();
 
         $transactions = Transaction::where('user_id', $user->id)
-            ->orderBy('created_at', 'desc')
-            ->get();
+        ->orderBy('created_at', 'desc')
+        ->paginate(5);
 
         return view('user.profile.riwayat', compact('transactions'));
     }
