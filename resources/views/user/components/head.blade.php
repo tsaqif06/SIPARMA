a91325185a68a0a8285e9b7009c2a25b124a7233
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2025-03-20 20:59:30 +0700

    fix midtrans server

--- resources/views/user/components/head.blade.php
+++ resources/views/user/components/head.blade.php
@@ -26,8 +26,8 @@
     <link rel="stylesheet" href="{{ asset('assets/css/lib/leaflet-routing-machine.css') }}">
 
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-    <script src="https://app.sandbox.midtrans.com/snap/snap.js"
+    <script
+        src="{{ config('services.midtrans.is_production') ? 'https://app.midtrans.com/snap/snap.js' : 'https://app.sandbox.midtrans.com/snap/snap.js' }}"
         data-client-key="{{ config('services.midtrans.client_key') }}"></script>
-
     <style></style>
 </head>
